b168d7846fb8659203f61113a2f999571b3f07d2
ni3choudhary <chnitin012@gmail.com>
2023-04-02 22:56:34 +0530

    updated data ingestion code with data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts',"train.csv")
@@ -42,8 +44,12 @@ class DataIngestion:
 
             )
         except Exception as e:
+            logging.info(CustomException(e,sys))
             raise CustomException(e,sys)
         
 if __name__=="__main__":
     data_ingestion = DataIngestion()
     train_data,test_data = data_ingestion.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
