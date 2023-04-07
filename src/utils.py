17f3c3ad927f040fc5676188516c9c3efde87f23
ni3choudhary <chnitin012@gmail.com>
2023-04-08 00:41:19 +0530

    Added load_object function to load preprocessor and model pickled file

--- src/utils.py
+++ src/utils.py
@@ -57,3 +57,13 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     except Exception as e:
         logging.exception(CustomException(e,sys))
         raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        logging.exception(CustomException(e,sys))
+        raise CustomException(e, sys)
